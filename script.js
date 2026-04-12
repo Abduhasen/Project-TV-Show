2fdd73162fd9ee42649e451a665a71f24284d6f3
abduhasen <abdumussa1363@gmail.com>
2026-04-12 22:50:44 +0100

    working on level 100 and style change

--- script.js
+++ script.js
@@ -6,7 +6,21 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  const cards = episodeList.map((episode) => createEpisodeCard(episode));
+  rootElem.append(...cards);
 }
-
+function createEpisodeCard(episode) {
+  const card = document.createElement("article");
+  const episodeCode = `S${episode.season.toString().padStart(2, "0")}E${episode.number.toString().padStart(2, "0")}`;
+  const title = document.createElement("h3");
+  title.textContent = `${episode.name} - ${episodeCode}`;
+  card.append(title);
+  const imageOfEpisode = document.createElement("img");
+  imageOfEpisode.src = episode.image.medium;
+  card.append(imageOfEpisode);
+  const summary = document.createElement("p");
+  summary.innerHTML = episode.summary;
+  card.append(summary);
+  return card;
+}
 window.onload = setup;
